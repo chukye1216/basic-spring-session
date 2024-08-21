1ffcff1738993e50e2fac2de12fed121f3d9237b
chukye1216 <chukye1216@gmail.com>
2024-08-21 17:29:17 +0900

    기본구조 세팅

---
+++ src/main/java/com/sparta/basicspringsession/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.sparta.basicspringsession.controller;
+
+import com.sparta.basicspringsession.dto.MemberSaveRequestDto;
+import com.sparta.basicspringsession.dto.MemberSaveResponseDto;
+import com.sparta.basicspringsession.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+}
---
+++ src/main/java/com/sparta/basicspringsession/entitiy/Member.java
@@ -0,0 +1,18 @@
+package com.sparta.basicspringsession.entitiy;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
